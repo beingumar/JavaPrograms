acf40a5c92df891eb34ebd785db95773c1d5f85a
UmarShaik <32830883+beingumar@users.noreply.github.com>
2023-04-17 17:31:07 +0530

    Assignment 1
    This is a calculator java program with static variables

---
+++ CalcAssignment - 1/Calc.java
@@ -0,0 +1,16 @@
+
+public class Calc {
+
+	public static void main(String[] args) 
+	{
+		Maths m = new Maths();
+	    int a= 100;
+	    int b= 200;
+	    m.add(a,b);
+	    m.sub(a, b);
+	    m.square(5);
+	    m.cube(10);
+	    m.div(10, 2);
+	}
+
+}
---
+++ CalcAssignment - 1/Maths.java
@@ -0,0 +1,25 @@
+public class Maths {
+  public void add(int a, int b) {
+    System.out.println("The addition is: "+a+b);
+  }
+
+  public void sub(int a, int b) {
+    System.out.println("The Subtraction is:"+ (a-b));
+  }
+
+  public void mul(int a, int b) {
+    System.out.println("The Multiplication is: "+(a*b));
+  }
+
+  public void div(int a, int b) {
+    System.out.println("The Division is: "+(a / b));
+  }
+
+  public void square(int a) {
+    System.out.println("The square of" +a+ "is :"+(a*a));
+  }
+
+  public void cube(int a) {
+    System.out.println("The Cube of "+a+" is: "+( a * a * a));
+  }
+}
